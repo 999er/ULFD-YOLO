2e76bfa21b66ef10f188f3213716832d4b89fe44
999er <2147537433@qq.com>
2026-07-16 14:14:28 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-@Auth ： 挂科边缘
-@File ：trian.py
-@IDE ：PyCharm
-@Motto:学习新思想，争做新青年
-@Email ：179958974@qq.com
-"""
 import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
